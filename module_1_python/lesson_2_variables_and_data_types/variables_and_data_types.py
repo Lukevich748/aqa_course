6ee97d9f30f8a135cfcdc411655486b50f25ef70
Lukevich <luckevichartem@gmail.com>
2024-04-10 22:32:24 +0200

    variables and data types

---
+++ module_1_python/lesson_2_variables_and_data_types/variables_and_data_types.py
@@ -0,0 +1,31 @@
+# 1
+age = 29
+name = "Artem"
+is_student = False
+temperature = 9.1
+print(age, name, is_student, temperature)
+age = str(age)
+temperature = int(temperature)
+
+# 2
+x = 10
+x = 20
+
+a = 5
+b = 10
+print(a + b,
+      b - a,
+      a * b,
+      a / b,
+      a ** b,
+      a % b,
+      )
+
+print(
+    a == b,
+    a != b,
+    a > b,
+    a < b,
+    a >= b,
+    a <= b
+)
