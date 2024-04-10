ab585da3307c49fe18486da521324951701b0b18
Lukevich <luckevichartem@gmail.com>
2024-04-10 22:33:03 +0200

    string

---
+++ module_1_python/lesson_3_string/string.py
@@ -0,0 +1,24 @@
+# 1
+text = "Hello Alexey"
+print(text[0])
+print(text[-1])
+print(text[1:3])
+print(len(text))
+
+# 2
+name = "Артем"
+age = 29
+message = "Меня зовут " + name + " и мне " + str(age) + " лет."
+message_f = f"Меня зовут {name} и мне {age} лет"
+text_to_escape = "\"Погода\" прекрасная, сегодня '+25' \\все идем гулять\\"
+
+# 3
+text = "Python - лучший язык для автоматизации!"
+contains_python = "Python" in text
+text_2 = "Мы работаем, чтобы стать лучше"
+new_text_2 = text_2.replace(" ", "*")
+contains = "!" in text[-1]
+text_upper = text.upper()
+
+
+
