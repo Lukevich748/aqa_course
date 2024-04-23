64ce62b3542908174e6b1ba10e5f417430ec6e6b
Lukevich <luckevichartem@gmail.com>
2024-04-23 12:21:46 +0300

    func changes #2

--- module_1_python/lesson_9_func/func.py
+++ module_1_python/lesson_9_func/func.py
@@ -11,21 +11,21 @@ def greeting(name):
     return f"Hello {name}!"
 
 
-greeting_result = greeting("Artem")
+greeting_result = greeting(name="Artem")
 print(greeting_result)
 
 
 # 2
 def get_movie_ticket(full_name: str, age: int, adult=False):
     if age >= 18:
-        return f"{full_name}, Билет продан."
+        return "Билет продан."
     elif adult:
-        return f"{full_name}, Билет продан под присмотром взрослого."
+        return "Билет продан под присмотром взрослого."
     else:
-        return f"{full_name}, Нельзя продать билет."
+        return "Нельзя продать билет."
 
 
-ticket_result = get_movie_ticket("Lukevich Artem", 17, True)
+ticket_result = get_movie_ticket(full_name="Lukevich Artem", age=17, adult=True)
 print(ticket_result)
 
 
@@ -33,3 +33,4 @@ print(ticket_result)
 def get_square_list(numbers: list):
     square_list = [number ** 2 for number in numbers]
     return square_list
+
